bf2788d057c759135879db00b24612deb4f52933
maxgnt <155428888+maxgnt@users.noreply.github.com>
2025-04-22 16:04:37 +0200

    update on time

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ return (
         <h1 className="font-bold">Bienvenue dans votre TodoList</h1>" 
         <p  className="mb-6">Gèrer mes tâches quotidiennes</p>
 
-    <button onClick={onstart
+    <button onClick={onstart}
 
 
 
